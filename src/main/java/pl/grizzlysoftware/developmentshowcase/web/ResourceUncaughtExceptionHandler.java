0a35f61c0fa032ef933a6b24bb1c27846f85de2e
BartekPawlowski <bartosz.pawlowski@connectpoint.pl>
2020-08-11 18:38:17 +0200

    Added logging and getters

--- src/main/java/pl/grizzlysoftware/developmentshowcase/web/ResourceUncaughtExceptionHandler.java
+++ src/main/java/pl/grizzlysoftware/developmentshowcase/web/ResourceUncaughtExceptionHandler.java
@@ -1,5 +1,6 @@
 package pl.grizzlysoftware.developmentshowcase.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,11 +11,12 @@ import pl.grizzlysoftware.developmentshowcase.businesslogic.exception.GetNumberE
  * Created by Bartosz Pawłowski on 10/08/2020.
  */
 @ControllerAdvice
+@Slf4j
 public class ResourceUncaughtExceptionHandler {
 
     @ExceptionHandler(GetNumberException.class)
     @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
-    public void handleGetNumberException() {
-
+    public void handleGetNumberException(GetNumberException e) {
+        log.warn("Uncaught exception while fetching numbers: {}", e.getMessage());
     }
 }
